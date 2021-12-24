4ba23b867daea222532d9915c5e7c8d00dd2bd40
tategotoazarasi <2724167997@qq.com>
2021-12-24 09:35:44 +0800

    add test

--- excel_sheet_column_title.cpp
+++ excel_sheet_column_title.cpp
@@ -4,27 +4,29 @@ using namespace excel_sheet_column_title;
 using std::string;
 
 namespace excel_sheet_column_title {
-    [[maybe_unused]] string Solution::convertToTitle(int columnNumber) {
-        string ans = string();
-        bool round = false;
-        while (columnNumber != 0) {
-            char ch;
-            if (round) {
-                ch = static_cast<char>(columnNumber % 26 + 63);
-                round = false;
-            } else ch = static_cast<char>(columnNumber % 26 + 64);
-            if (ch == '@' && columnNumber >= 26) {
-                ch = 'Z';
-                round = true;
-            } else if (ch == '?' && columnNumber >= 26) {
-                ch = 'Y';
-                round = true;
-            }
-            if ('A' <= ch && ch <= 'Z') {
-                ans.insert(0, 1, ch);
-            }
-            columnNumber /= 26;
-        }
-        return ans;
-    }
+	[[maybe_unused]] string Solution::convertToTitle(int columnNumber) {
+		string ans = string();
+		bool round = false;
+		while (columnNumber != 0) {
+			char ch;
+			if (round) {
+				ch = static_cast<char>(columnNumber % 26 + 63);
+				round = false;
+			}
+			else ch = static_cast<char>(columnNumber % 26 + 64);
+			if (ch == '@' && columnNumber >= 26) {
+				ch = 'Z';
+				round = true;
+			}
+			else if (ch == '?' && columnNumber >= 26) {
+				ch = 'Y';
+				round = true;
+			}
+			if ('A' <= ch && ch <= 'Z') {
+				ans.insert(0, 1, ch);
+			}
+			columnNumber /= 26;
+		}
+		return ans;
+	}
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 
-int main(int argc, char **argv) {
-    testing::InitGoogleTest();
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
 }
