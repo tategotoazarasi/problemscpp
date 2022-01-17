f124579b3e7f7afc5f50e7c4b4371f0b362be686
Tategoto Azarasi <2724167997@qq.com>
2022-01-17 09:02:06 +0800

    洛谷 P1000 超级玛丽游戏
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- luogu.h
+++ luogu.h
@@ -11,6 +11,12 @@ namespace luogu {
 	public:
 		static int main(istream &cin, ostream &cout);
 	};
+
+	/// \brief 洛谷 P1000 超级玛丽游戏
+	class P1000 {
+	public:
+		static int main(istream &cin, ostream &cout);
+	};
 }// namespace luogu
 
 #endif//PROBLEMSCPP_LUOGU_H
