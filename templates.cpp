cef74f7858428d9522577daee96c567dde2700f9
Tategoto Azarasi <2724167997@qq.com>
2022-03-22 14:45:48 +0800

    BigInt init
    Signed-off-by: Tategoto Azarasi <2724167997@qq.com>

--- templates.cpp
+++ templates.cpp
@@ -1,4 +1,7 @@
 #include "templates.h"
+#include <sstream>
+
+using namespace std;
 
 void TrieNode::insert(const string &str) {
 	if(this->nexts[str[0] - 'a'] == nullptr) {
@@ -15,4 +18,324 @@ TrieNode::~TrieNode() {
 	for(const auto *next: this->nexts) {
 		delete next;
 	}
+}
+
+unsigned long BigInt::get_size() const {
+	return vec.size();
+}
+
+unsigned short BigInt::operator[](unsigned long i) const {
+	return vec[i];
+}
+
+BigInt::BigInt(short n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(int n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(long n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(long long int n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(const unsigned short &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(const unsigned int &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(const unsigned long &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(const unsigned long long int &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+}
+
+BigInt::BigInt(const string &str) {
+	if(str[0] == '-') {
+		positive = false;
+		vec      = vector<unsigned short>(str.length() - 1);
+		for(int i = 1; i < str.length(); i++) {
+			vec[i - 1] = str[i] - '0';
+		}
+	} else {
+		vec = vector<unsigned short>(str.length());
+		for(int i = 0; i < str.length(); i++) {
+			vec[i] = str[i] - '0';
+		}
+	}
+}
+
+BigInt::BigInt(const BigInt &bi) {
+	this->vec      = vector(bi.vec);
+	this->positive = bi.positive;
+}
+
+BigInt &BigInt::operator=(const BigInt &bi) {
+	this->vec      = vector(bi.vec);
+	this->positive = bi.positive;
+	return *this;
+}
+
+BigInt &BigInt::operator=(const string &str) {
+	if(str[0] == '-') {
+		positive = false;
+		vec      = vector<unsigned short>(str.length() - 1);
+		for(int i = 1; i < str.length(); i++) {
+			vec[i - 1] = str[i] - '0';
+		}
+	} else {
+		vec = vector<unsigned short>(str.length());
+		for(int i = 0; i < str.length(); i++) {
+			vec[i] = str[i] - '0';
+		}
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(int n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(short n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(long n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(long long int n) {
+	if(n < 0) {
+		positive = false;
+		n        = -n;
+	}
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(const unsigned short &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(const unsigned int &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(const unsigned long &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt &BigInt::operator=(const unsigned long long int &n) {
+	vec = vector<unsigned short>();
+	stringstream ss;
+	ss << n;
+	char ch;
+	while(ss >> ch) {
+		vec.push_back(ch - '0');
+	}
+	return *this;
+}
+
+BigInt BigInt::operator+(const BigInt &bi) const {
+	return *this;
+}
+
+BigInt BigInt::operator-(const BigInt &bi) const {
+	return *this;
+}
+
+BigInt BigInt::operator*(const BigInt &bi) const {
+	return *this;
+}
+
+BigInt BigInt::operator/(const BigInt &bi) const {
+	return *this;
+}
+
+BigInt BigInt::operator%(const BigInt &bi) const {
+	return *this;
+}
+
+bool BigInt::operator>(const BigInt &bi) const {
+	return true;
+}
+
+bool BigInt::operator<(const BigInt &bi) const {
+	return true;
+}
+
+bool BigInt::operator==(const BigInt &bi) const {
+	return true;
+}
+
+bool BigInt::operator!=(const BigInt &bi) const {
+	return true;
+}
+
+bool BigInt::operator>=(const BigInt &bi) const {
+	return true;
+}
+
+bool BigInt::operator<=(const BigInt &bi) const {
+	return true;
+}
+
+BigInt &BigInt::operator+=(const BigInt &bi) {
+	return *this;
+}
+
+BigInt &BigInt::operator-=(const BigInt &bi) {
+	return *this;
+}
+
+BigInt &BigInt::operator*=(const BigInt &bi) {
+	return *this;
+}
+
+BigInt &BigInt::operator/=(const BigInt &bi) {
+	return *this;
+}
+
+BigInt &BigInt::operator%=(const BigInt &bi) {
+	return *this;
+}
+
+ostream &operator<<(ostream &os, const BigInt &) {
+}
+
+istream &operator>>(istream &is, const BigInt &) {
 }
--- templates.h
+++ templates.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <array>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -15,4 +16,54 @@ struct TrieNode {
 
 	void insert(const string &str);
 	~TrieNode();
+};
+
+/// \brief 高精度整数
+class BigInt {
+private:
+	bool positive              = true;
+	vector<unsigned short> vec = {};
+	unsigned long get_size() const;
+	unsigned short operator[](unsigned long) const;
+	BigInt(const vector<unsigned short> &vec, bool positive): vec(vec), positive(positive){};
+
+public:
+	BigInt(short n);
+	BigInt(int n);
+	BigInt(long n);
+	BigInt(long long n);
+	BigInt(const unsigned short &n);
+	BigInt(const unsigned int &n);
+	BigInt(const unsigned long &n);
+	BigInt(const unsigned long long &n);
+	BigInt(const string &str);
+	BigInt(const BigInt &bi);
+	BigInt &operator=(const BigInt &bi);
+	BigInt &operator=(const string &str);
+	BigInt &operator=(int n);
+	BigInt &operator=(short n);
+	BigInt &operator=(long n);
+	BigInt &operator=(long long n);
+	BigInt &operator=(const unsigned short &n);
+	BigInt &operator=(const unsigned int &n);
+	BigInt &operator=(const unsigned long &n);
+	BigInt &operator=(const unsigned long long &n);
+	BigInt operator+(const BigInt &bi) const;
+	BigInt operator-(const BigInt &bi) const;
+	BigInt operator*(const BigInt &bi) const;
+	BigInt operator/(const BigInt &bi) const;
+	BigInt operator%(const BigInt &bi) const;
+	BigInt &operator+=(const BigInt &bi);
+	BigInt &operator-=(const BigInt &bi);
+	BigInt &operator*=(const BigInt &bi);
+	BigInt &operator/=(const BigInt &bi);
+	BigInt &operator%=(const BigInt &bi);
+	bool operator>(const BigInt &bi) const;
+	bool operator<(const BigInt &bi) const;
+	bool operator==(const BigInt &bi) const;
+	bool operator!=(const BigInt &bi) const;
+	bool operator>=(const BigInt &bi) const;
+	bool operator<=(const BigInt &bi) const;
+	friend ostream &operator<<(ostream &os, const BigInt &);
+	friend istream &operator>>(istream &is, const BigInt &);
 };
